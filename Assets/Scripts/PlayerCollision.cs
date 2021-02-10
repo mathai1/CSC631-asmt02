5127476c7fa84f5b1dab644eb570c4cea22343e8
mathai1 <mthai1@mail.sfsu.edu>
2021-02-10 12:51:15 -0800

    Fixed Map Location & Connected the Two scenes upon collision with gate

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag=="Gate")
+        {
+           FindObjectOfType<LevelManager>().nextLevel();
+        }
+    }
+    
+}
