66b68b02501d3d5ad1deba413fe6f8007bb0bc80
mathai1 <mthai1@mail.sfsu.edu>
2021-02-09 17:05:06 -0800

    Base Layer Initial Commit

---
+++ Assets/PlayerAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public Rigidbody rb;
+
+   public float speed = 50;
+ 
+     void Update () {
+         Vector3 pos = transform.position;
+ 
+         if (Input.GetKey ("w")) {
+             pos.z += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("s")) {
+             pos.z -= speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("d")) {
+             pos.x += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("a")) {
+             pos.x -= speed * Time.deltaTime;
+         }
+         transform.position=pos;    
+     }
+}
