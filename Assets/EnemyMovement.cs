1c77dd28cc5c1ea68d97c975cf639ae8682b279b
mathai1 <mthai1@mail.sfsu.edu>
2021-02-10 15:07:36 -0800

    Added comments to Enemy Movement Script

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    //Code From: https://www.youtube.com/watch?v=4Wh22ynlLyk
     public Transform player;
     private Rigidbody rb;
     private Vector3 movement;
@@ -17,6 +18,7 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //finds the angle between  where the enemy is currently facing and the player
         Vector3 direction =player.position-transform.position;
         float angle = Mathf.Atan2(direction.y,direction.x)* Mathf.Rad2Deg;
         rb.rotation =  Quaternion.Euler (new Vector3(0f,angle,0f));
