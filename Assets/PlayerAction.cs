6f617c8401cac6637b4d9b06ef154f4f90dec190
mathai1 <mthai1@mail.sfsu.edu>
2021-02-10 09:50:09 -0800

    Added Character Rotation to Player Object

--- Assets/PlayerAction.cs
+++ Assets/PlayerAction.cs
@@ -9,12 +9,33 @@ public class PlayerAction : MonoBehaviour
     public int speed;
 
     // Update is called once per frame
-    // Code from: https://www.codegrepper.com/code-examples/csharp/unity+wasd+movement
-    //movement using wasd or arrow keys
+    
     void FixedUpdate()
     {
+        // Code from: https://www.codegrepper.com/code-examples/csharp/unity+wasd+movement
+        //movement using wasd or arrow keys
         Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         this.transform.position += Movement * speed * Time.deltaTime;
     }
+    void Update () 
+     {
+         
+         //Get the Screen positions of the object
+         Vector2 positionOnScreen = UnityEngine.Camera.main.WorldToViewportPoint (transform.position);
+         
+         //Get the Screen position of the mouse
+         Vector2 mouseOnScreen = (Vector2)UnityEngine.Camera.main.ScreenToViewportPoint(Input.mousePosition);
+         
+         //Get the angle between the points
+         float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+ 
+         //Ta Daaa
+         transform.rotation =  Quaternion.Euler (new Vector3(0f,angle,0f));
+     }
+ 
+     float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+         return Mathf.Atan2(a.x -b.x, a.y -b.y) * Mathf.Rad2Deg;
+     }
+
     
 }
