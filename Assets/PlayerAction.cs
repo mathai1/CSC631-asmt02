90cafbd749edc1042bdc0e4767f72cbe8d8abb4c
mathai1 <mthai1@mail.sfsu.edu>
2021-02-09 18:01:14 -0800

    Updated player movement script

--- Assets/PlayerAction.cs
+++ Assets/PlayerAction.cs
@@ -6,23 +6,14 @@ public class PlayerAction : MonoBehaviour
 {
     public Rigidbody rb;
 
-   public float speed = 50;
- 
-     void Update () {
-         Vector3 pos = transform.position;
- 
-         if (Input.GetKey ("w")) {
-             pos.z += speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("s")) {
-             pos.z -= speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("d")) {
-             pos.x += speed * Time.deltaTime;
-         }
-         if (Input.GetKey ("a")) {
-             pos.x -= speed * Time.deltaTime;
-         }
-         transform.position=pos;    
-     }
+    public int speed;
+
+    // Update is called once per frame
+    // Code from: https://www.codegrepper.com/code-examples/csharp/unity+wasd+movement
+    //movement using wasd or arrow keys
+    void FixedUpdate()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        this.transform.position += Movement * speed * Time.deltaTime;
+    }
 }
