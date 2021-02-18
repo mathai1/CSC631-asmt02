cab3ab6d8f29f2d0b2f7a8750cee1a61f0f4dd85
Janson Leong <jaleong61599@gmail.com>
2021-02-17 16:27:58 -0800

    added swapping day and night

---
+++ Assets/Scripts/LightRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+    float change = -90f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateLight();
+//        transform.RotateAround(Vector3.zero,Vector3.right,6f*Time.deltaTime);
+//        transform.LookAt(Vector3.zero);
+    }
+
+    void rotateLight()
+    {
+        //Hit F to swap day and night
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            transform.Rotate(new Vector3(change,0f,0f));
+            if (change > 0f) change -= 180f;
+            else change += 180f;
+        }
+    }
+}
