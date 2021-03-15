54c533c2afa07f84ed93668d500c79e297c98c39
mathai1 <mthai1@mail.sfsu.edu>
2021-03-15 13:42:47 -0700

    Added Second Camera/ Updated UI

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,18 +6,24 @@ public class Camera : MonoBehaviour
 {
     public GameObject player1;
     public GameObject player2;
+    public GameObject player1camera;
+    public GameObject player2camera;
     public Transform player;
     void Start()
     {
         //get the character that player chooses during character selection
         if (PlayerPrefs.GetString("Player")=="Player1"){
             player1.SetActive(true);
+            player1camera.SetActive(true);
             player2.SetActive(false);
+            player2camera.SetActive(false);
             player=player1.transform;
         }
         if (PlayerPrefs.GetString("Player")=="Player2"){
             player2.SetActive(true);
+            player2camera.SetActive(true);
             player1.SetActive(false);
+            player1camera.SetActive(false);
             player=player2.transform;
         }
     }
